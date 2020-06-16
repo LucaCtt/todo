cb5dcd171df4d8ee820cbd3334a5798857712bb7
Luca Cotti <lucacotti@outlook.com>
2020-06-17 00:10:38 +0200

    Adds amplify

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Amplify from "aws-amplify";
 import "mobx-react-lite/batchingForReactNative";
 
+import config from "./aws-exports";
 import AppNavigator from "./navigation/AppNavigator";
 import UIProvider from "./components/UIProvider";
 import StoreProvider from "./store/StoreProvider";
+
+Amplify.configure(config);
 
 export default function App() {
   return (
