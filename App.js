90b450a97034ac6039274f32824c33cf3a1c3cfa
Luca Cotti <lucacotti@outlook.com>
2020-06-20 19:47:41 +0200

    Updates auth handling

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import React from "react";
 import Amplify from "aws-amplify";
 import "mobx-react-lite/batchingForReactNative";
 
-import config from "./aws-exports";
+import awsconfig from "./aws-exports";
 import AppNavigator from "./navigation/AppNavigator";
 import UIProvider from "./components/UIProvider";
 import StoreProvider from "./store/StoreProvider";
 
-Amplify.configure(config);
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
 export default function App() {
   return (
